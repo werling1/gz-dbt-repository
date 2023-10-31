7e0b8a6ccfc84129362352afd682c83feae9365f
werling.julius@googlemail.com <werling.julius@googlemail.com>
2023-10-31 11:56:23 +0000

    post exercise 7 and 8

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        ship_cost
+
+    from source
+
+)
+
+select * from renamed
+
